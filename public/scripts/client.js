95eeefa0fbaafa84a36737f662e12b016c8ec04d
Anna Gordon <annetta@email.com>
2020-02-26 01:57:00 +0000

    Compose-char-counter function, sttic version of article-tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,14 +16,33 @@ const tweetData = {
 }
 
 
-// const createTweetElement = (object) {
-
-// }
+const createTweetElement = (object) => {
+  console.log( object)
+  let name = object.user.name;
+  console.log(object.user.name)
+  
+}
+const markup = `
+<header>
+  <div>
+    <p class="name">${tweetData.user.name}</p>
+    </div>
+    </header>
+    `
+    $('.name').html('<p>' + tweetData.user.name + '</p>');
+// document.body.innerHTML = markup;
+    // <p class="location">${person.location}</p>
+    // <p class="bio">${person.bio}</p>
+    
+    /* <img src="https://i.imgur.com/73hZDYK.png"> 
+            <p class="name">Newton</p>
+          </div>
+          <span>@Sirlsaac</span> */
 
 
 const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
 console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); 
+$('.tweets-container').append($tweet); 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,29 +1,32 @@
-$(document).ready(function() {
+$(document).ready(() => {
 
-  $(function() {
-    let input = $('textarea');
-    let display = $('.counter'); 
-    let limit = 10;
-    let count = 0;
-    
-    count = input.val().length;
-    let remaining = limit - count;
+  // let limit = 140;
+  // $('textarea').keyup(function() {
+  //   count = $(this).val().length;
+  //   let remaining = limit - count;
+  //   update(remaining);
+  // });
+
+  
+  let limit = 10;
+  
+  let count = $('textarea').val().length;
+  let remaining = limit - count;
+  update(remaining);
+
+  $('textarea').keyup(function(event) {
+    count = $(this).val().length;
+    remaining = limit - count;
+
     update(remaining);
-  
-    input.keyup(function(e) {
-      count = $(this).val().length;
-      remaining = limit - count;
-  
-      update(remaining);
-    });
-  
-    function update(count) {
-      if (count < 0) {
-        display.css('color', 'red');
-      }
-        display.html(count);
-
-    }
-  
   });
+
+  function update(count) {
+    if (count < 0) {
+      $('.counter').html(count).css('color', 'red');
+    } else {
+      $('.counter').html(count).css('color', '#545149');
+    }
+  }
+
 });
